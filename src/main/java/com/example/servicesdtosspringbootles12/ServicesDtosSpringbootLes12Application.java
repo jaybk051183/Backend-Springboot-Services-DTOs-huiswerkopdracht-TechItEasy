0a2e76dbe60403c01a3b486fe2d7066a81ae37b2
jaybk051183 <kailashbhaggoe@outlook.com>
2023-03-05 19:05:37 +0100

    project initiation

---
+++ src/main/java/com/example/servicesdtosspringbootles12/ServicesDtosSpringbootLes12Application.java
@@ -0,0 +1,13 @@
+package com.example.servicesdtosspringbootles12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesDtosSpringbootLes12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesDtosSpringbootLes12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/servicesdtosspringbootles12/ServicesDtosSpringbootLes12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicesdtosspringbootles12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesDtosSpringbootLes12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
